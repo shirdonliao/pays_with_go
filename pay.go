7903cc12462c68bc5f884e2bd237696336921ffd
shirdonliao <toingart@163.com>
2018-11-27 15:17:40 +0800

    支付库调试上传

---
+++ pay.go
@@ -0,0 +1,66 @@
+package pays_with_go
+
+import (
+	"errors"
+	"./client"
+	"./common"
+	"./constant"
+	"strconv"
+)
+//pay
+func do(charge *common.Charge) (string, error) {
+	err := checkCharge(charge)
+	if err != nil {
+		//log.Error(err, charge)
+		return "", err
+	}
+
+	ct := getPayClient(charge.PayMethod)
+	re, err := ct.Pay(charge)
+	if err != nil {
+		//log.Error("支付失败:", err, charge)
+		return "", err
+	}
+	return re, err
+}
+
+func checkCharge(charge *common.Charge) error {
+	var id uint64
+	var err error
+	if charge.UserID == "" {
+		id = 0
+	} else {
+		id, err = strconv.ParseUint(charge.UserID, 10, -1)
+		if err != nil {
+			return err
+		}
+	}
+	if id < 0 {
+		return errors.New("userID less than 0")
+	}
+	if charge.PayMethod < 0 {
+		return errors.New("payMethod less than 0")
+	}
+	if charge.MoneyFee < 0 {
+		return errors.New("totalFee less than 0")
+	}
+
+	if charge.CallbackURL == "" {
+		return errors.New("callbackURL is NULL")
+	}
+	return nil
+}
+
+// getPayClient 得到需要支付的客户端
+func getPayClient(payMethod int64) common.PayClient {
+	//如果使用余额支付
+	switch payMethod {
+	case constant.ALI_WEB:
+		return client.DefaultAliWebClient()
+	case constant.ALI_APP:
+		return client.DefaultAliAppClient()
+	case constant.WECHAT:
+		return client.DefaultWechatAppClient()
+	}
+	return nil
+}
---
+++ pay_test.go
@@ -0,0 +1,55 @@
+package pays_with_go
+
+import (
+	"fmt"
+	"./client"
+	"./common"
+	"./constant"
+	"net/http"
+	"testing"
+)
+
+func TestPay(t *testing.T) {
+	initClient()
+	initHandle()
+	charge := new(common.Charge)
+	charge.PayMethod = constant.WECHAT
+	charge.MoneyFee = 1
+	charge.Describe = "test pay"
+	charge.TradeNum = "8888888888"
+
+	fdata, err := do(charge)
+	if err != nil {
+		t.Error(err)
+	}
+	fmt.Println(fdata)
+}
+
+
+//初始化客户端
+func initClient() {
+	client.InitAliAppClient(&client.AliAppClient{
+		PartnerID:  "xxx",
+		SellerID:   "xxxx",
+		AppID:      "xxx",
+		PrivateKey: nil,
+		PublicKey:  nil,
+	})
+}
+
+//初始化处理
+func initHandle() {
+	http.HandleFunc("callback/aliappcallback", func(w http.ResponseWriter, r *http.Request) {
+		aliResult, err := AliAppCallback(w, r)
+		if err != nil {
+			fmt.Println(err)
+			//log.xxx
+			return
+		}
+		selfHandler(aliResult)
+	})
+}
+
+func selfHandler(i interface{}) {
+}
+
